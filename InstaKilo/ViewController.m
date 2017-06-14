27a6f54dc6082c672ff91f5d5ee13c940c606eab
nekomeo <linh.tu545@gmail.com>
2017-06-14 08:51:17 -0700

    Initial Commit

---
+++ InstaKilo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InstaKilo
+//
+//  Created by Elle Ti on 2017-06-14.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
